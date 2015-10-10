4275b7b60ad926db413299b34ff4a061acab1036
Jorge Díaz <jorgedf@gmail.com>
2015-10-10 10:11:39 +0200

    Enumerable/Hash extends for parsing purposes.

--- lib/core_ext/hash.rb
+++ lib/core_ext/hash.rb
@@ -1,5 +1,10 @@
 class Hash
 
+  def deep_symbolize_keys!
+    deep_transform_keys!{ |key| key.to_sym rescue key }
+  end
+
+
   def deep_transform_keys!(&block)
     keys.each do |key|
       value = delete(key)
---
+++ lib/core_ext/nested_enumerables.rb
@@ -0,0 +1,33 @@
+# Included from https://github.com/chengguangnan/vine to avoid dependency
+
+module Enumerable
+
+  def segmentation(n)
+    0.upto(size - 2).to_a.combination(n - 1).to_a.map do |s|
+      [-1, * s, size - 1].each_cons(2).map {|i, j| self[(i + 1)..j] }
+    end
+  end
+
+end
+
+
+class Hash
+
+  def access(path)
+    value = self
+
+    path.to_s.split('/').each do |p|
+      if p.to_i.to_s == p
+        value = value[p.to_i]
+      else
+        value = value[p].nil? ? value[p.to_sym] : value[p]
+      end
+      break if value.nil?
+    end
+
+    value
+  end
+
+  alias :hpath :access
+
+end
