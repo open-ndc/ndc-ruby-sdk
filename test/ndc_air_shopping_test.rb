84222d68a5db19b29d549ae41c857fdd45f11fe2
Jorge Díaz <jorgedf@gmail.com>
2015-10-10 10:17:34 +0200

    Functional/integration testing improved for AirShopping message. Valid and invalid requests simulated.

--- test/ndc_air_shopping_test.rb
+++ test/ndc_air_shopping_test.rb
@@ -3,41 +3,105 @@ require 'test_helper'
 class NDCAirShoppingTest < Test::Unit::TestCase
   extend Minitest::Spec::DSL
 
-  ndc_config = YAML.load_file('test/config/ndc-iata-kronos.yml')
-  @@ndc_client = NDCClient::Base.new(ndc_config)
-  query_params = {departure_airport_code: 'MUC', arrival_airport_code: 'LHR', departure_date: '2016-04-01'}
-  @@ndc_response = @@ndc_client.request(:AirShopping, query_params)
+  describe "Sends an invalid AirShopping request" do
 
-  setup do
+    ndc_config = YAML.load_file('test/config/ndc-iata-kronos.yml')
+    @@ndc_client = NDCClient::Base.new(ndc_config)
+
+    query_params = {
+      CoreQuery: {
+        OriginDestinations: {
+          OriginDestination: {
+            Departure: {
+              AirportCode: 'MUC'
+            },
+            Arrival: {
+              AirportCode: 'LHR'
+            }
+          }
+        }
+      }
+    }
+
+    test "AirShopping request requires Departure/Date" do
+      assert_raises(NDCClient::NDCErrors::NDCInvalidResponseFormat) {
+        @@ndc_response = @@ndc_client.request(:AirShopping, query_params)
+      }
+    end
+
+    query_params = {
+      CoreQuery: {
+        OriginDestinations: {
+          OriginDestination: {
+            Departure: {
+              AirportCode: 'MUC',
+              Date: '2016-04-01'
+            },
+            Arrival: {
+            }
+          }
+        }
+      }
+    }
+
+    test "AirShopping request requires Arrival data" do
+      assert_raises(NDCClient::NDCErrors::NDCInvalidResponseFormat) {
+        @@ndc_response = @@ndc_client.request(:AirShopping, query_params)
+      }
+    end
+
   end
 
-  describe "Has a valid AirShopping response" do
+
+  describe "Sends a valid AirShopping request" do
+
+    ndc_config = YAML.load_file('test/config/ndc-iata-kronos.yml')
+    @@ndc_client = NDCClient::Base.new(ndc_config)
+
+    query_params = {
+      CoreQuery: {
+        OriginDestinations: {
+          OriginDestination: {
+            Departure: {
+              AirportCode: 'MUC',
+              Date: '2016-04-01'
+            },
+            Arrival: {
+              AirportCode: 'LHR'
+            }
+          }
+        }
+      }
+    }
+
+    @@ndc_response = @@ndc_client.request(:AirShopping, query_params)
+
 
     test "AirShopping request is valid" do
       assert @@ndc_client.valid_response?
     end
 
     test "Document version is ok" do
-      refute_nil @@ndc_response["AirShoppingRS"]["Document"]
-      assert_equal @@ndc_response["AirShoppingRS"]["Document"]["ReferenceVersion"], "1.0"
+      refute_empty @@ndc_response.hpath('AirShoppingRS/Document')
+      assert_equal @@ndc_response.hpath('AirShoppingRS/Document/ReferenceVersion'), "1.0"
     end
 
     test "ShoppingResponseID is ok" do
-      refute_nil @@ndc_response["AirShoppingRS"]["ShoppingResponseID"]
-      refute_nil @@ndc_response["AirShoppingRS"]["ShoppingResponseID"]["ResponseID"]
+      refute_empty @@ndc_response.hpath('AirShoppingRS/ShoppingResponseID')
+      refute_empty @@ndc_response.hpath('AirShoppingRS/ShoppingResponseID/ResponseID')
     end
 
     test "Airline Offer is present" do
-      refute_nil @@ndc_response["AirShoppingRS"]["OffersGroup"]["AirlineOffers"]
-      refute_empty @@ndc_response["AirShoppingRS"]["OffersGroup"]["AirlineOffers"]['TotalOfferQuantity']
+      refute_empty @@ndc_response.hpath('AirShoppingRS/OffersGroup/AirlineOffers')
+      refute_empty @@ndc_response.hpath('AirShoppingRS/OffersGroup/AirlineOffers/TotalOfferQuantity')
     end
 
     test "Airline Offers expiration are valid" do
-      @@ndc_response["AirShoppingRS"]["OffersGroup"]["AirlineOffers"].each{ |aoffer|
+      @@ndc_response.hpath('AirShoppingRS/OffersGroup/AirlineOffers').each{ |aoffer|
         if aoffer.first == "AirlineOffer"
           aoffer.last.each{|offer|
-            refute_nil offer["TimeLimits"]["OfferExpiration"]["@Timestamp"]
-            assert DateTime.parse(offer["TimeLimits"]["OfferExpiration"]["@Timestamp"]).to_time > Time.now
+            refute_nil offer.hpath('TimeLimits/OfferExpiration/@Timestamp')
+            assert DateTime.parse(offer.hpath('TimeLimits/OfferExpiration/@Timestamp')).to_time > Time.now
           }
         end
       }
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,6 @@
 require 'minitest/autorun'
 require 'test/unit'
-
 require 'pry'
-require 'pry-byebug'
 
 # Load test config
 require_relative '../lib/ndc-client'
