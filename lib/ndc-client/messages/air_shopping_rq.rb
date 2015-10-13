516cea83c594512a0753816cb08467a1d02b7a74
Jorge Díaz <jorgedf@gmail.com>
2015-10-13 06:34:58 +0200

    Major refactoring of NDC messages generation.

--- lib/ndc-client/messages/air_shopping_rq.rb
+++ lib/ndc-client/messages/air_shopping_rq.rb
@@ -5,109 +5,28 @@ module NDCClient
 
       def initialize(params = {})
         super (params)
-        data = @data
+      end
 
-        @message = Nokogiri::XML::Builder.new {|xml|
-                  xml.AirShoppingRQ(@namespaces) {
-                    xml.Document {
-                      xml.Name_ "NDC AirShoppingRQ Message"
-                      xml.MessageVersion_ "1.1.5"
-                      xml.ReferenceVersion_ "1.0"
-                    }
-                    xml.Party {
-                      xml.Sender {
-                        xml.TravelAgencySender {
-                          xml.Name_ data.hpath('Party/Sender/ORA_Sender/AgentUser/Name')
-                          xml.IATA_Number_ data.hpath('Party/Sender/ORA_Sender/AgentUser/IATA_Number')
-                          xml.AgencyID_ data.hpath('Party/Sender/ORA_Sender/AgentUser/AgentUserID')
-                        }
-                      }
-                      xml.Participants {
-                        xml.Participant {
-                          xml.AggregatorParticipant( SequenceNumber: 1){
-                            xml.Name_ data.hpath('Participants/Participant/AggregatorParticipant/Name')
-                            xml.AggregatorID_ data.hpath('Participants/Participant/AggregatorParticipant/AggregatorID')
-                          }
-                        }
-                      }
-                    }
-                    xml.Travelers {
-                      xml.Traveler {
-                        xml.AnonymousTraveler {
-                          xml.PTC(Quantity: 1).text "ADT"
-                        }
-                      }
-                    }
-                    xml.CoreQuery {
-                      xml.OriginDestinations {
-                        xml.OriginDestination {
-                          xml.Departure {
-                            xml.AirportCode_ data.hpath('CoreQuery/OriginDestinations/OriginDestination/Departure/AirportCode')
-                            xml.Date_ data.hpath('CoreQuery/OriginDestinations/OriginDestination/Departure/Date')
-                          }
-                          xml.Arrival {
-                            xml.AirportCode_ data.hpath('CoreQuery/OriginDestinations/OriginDestination/Arrival/AirportCode')
-                            xml.Date_ data.hpath('CoreQuery/OriginDestinations/OriginDestination/Arrival/Date') if data.hpath('CoreQuery/OriginDestinations/OriginDestination/Arrival/Date')
-                          }
-                        }
-                      }
-                    }
-                    # Missing Preferences Block - TEMPORARY FIX FOR JRT!
-                    xml.Preference {
-                      xml.AirlinePreferences {
-                        xml.Airline {
-                          xml.AirlineID_ data.hpath('Preference/AirlinePreferences/Airline/AirlineID')
-                        }
-                      }
-                      xml.FarePreferences {
-                        xml.Types {
-                          xml.Type {
-                            xml.Code_ data.hpath('Preference/FarePreferences/Types/Type/Code')
-                          }
-                        }
-                      }
-                      xml.CabinPreferences {
-                        xml.CabinType {
-                          xml.Code_ data.hpath('Preference/CabinPreferences/CabinType/Code')
-                          xml.Definition_ data.hpath('Preference/CabinPreferences/CabinType/Definition')
-                        }
-                      }
-                    }
-                    # } # TEMPORARY FIX FOR JRT!
-                    xml.Metadata {
-                      xml.Other {
-                        xml.OtherMetadata {
-                          xml.LanguageMetadatas {
-                            xml.LanguageMetadata(MetadataKey: "Display"){
-                              xml.Application_ "Display"
-                              xml.Code_ISO_ "en"
-                            }
-                          }
-                        }
-                      }
-                    }
-                  }
+      def yield_core_query(data, xml)
+        xml.CoreQuery {
+          xml.OriginDestinations {
+            xml.OriginDestination {
+              xml.Departure {
+                xml.AirportCode_ data.hpath('CoreQuery/OriginDestinations/OriginDestination/Departure/AirportCode')
+                xml.Date_ data.hpath('CoreQuery/OriginDestinations/OriginDestination/Departure/Date')
+              }
+              xml.Arrival {
+                xml.AirportCode_ data.hpath('CoreQuery/OriginDestinations/OriginDestination/Arrival/AirportCode')
+                xml.Date_ data.hpath('CoreQuery/OriginDestinations/OriginDestination/Arrival/Date') if data.hpath('CoreQuery/OriginDestinations/OriginDestination/Arrival/Date')
+              }
+              xml.MarketingCarrierAirline {
+                xml.AirlineID_ data.hpath('CoreQuery/OriginDestinations/OriginDestination/MarketingCarrierAirline/AirlineID')
+                xml.Name_ data.hpath('CoreQuery/OriginDestinations/OriginDestination/MarketingCarrierAirline/Name')
+              }
+            }
+          }
         }
       end
-
-      def to_xml
-        @message.doc.root.to_xml
-      end
-
-      # def wrap(wrapper)
-      #   @message.wrap(wrapper)
-      # end
-
-      # Soapy
-      # def to_xml_with_body_wrap
-      #   if @wrappers
-      #     @wrappers[:open] <<
-      #     @message.doc.root.to_xml <<
-      #     @wrappers[:close]
-      #   else
-      #     @message.doc.root.to_xml
-      #   end
-      # end
 
     end
 
--- lib/ndc-client/messages/base.rb
+++ lib/ndc-client/messages/base.rb
@@ -25,7 +25,123 @@ module NDCClient
         @ndc_config = Config.ndc_config.keep_if{|k,_| NDC_CONFIG_BLOCKS.include?(k)} if Config.valid?
         @params = params.keep_if{|k,_| NDC_PARAMS_BLOCKS.include?(k)} if params
         @data = (@ndc_config || {}).merge(@params)
+        build_message
       end
+
+
+      def build_message
+        data = @data
+        namespaces = @namespaces
+
+        @message = Nokogiri::XML::Builder.new {|xml|
+
+          xml.AirShoppingRQ(namespaces) {
+
+            xml.Document {
+              xml.Name_ "NDC AirShoppingRQ Message"
+              xml.MessageVersion_ "1.1.5"
+              xml.ReferenceVersion_ "1.0"
+            }
+
+            xml.Party {
+              xml.Sender {
+                xml.TravelAgencySender {
+                  xml.Name_ data.hpath('Party/Sender/ORA_Sender/AgentUser/Name')
+                  xml.IATA_Number_ data.hpath('Party/Sender/ORA_Sender/AgentUser/IATA_Number')
+                  xml.AgencyID_ data.hpath('Party/Sender/ORA_Sender/AgentUser/AgentUserID')
+                }
+              }
+              xml.Participants {
+                xml.Participant {
+                  xml.AggregatorParticipant( SequenceNumber: 1){
+                    xml.Name_ data.hpath('Participants/Participant/AggregatorParticipant/Name')
+                    xml.AggregatorID_ data.hpath('Participants/Participant/AggregatorParticipant/AggregatorID')
+                  }
+                }
+              }
+            }
+
+            yield_core_query(data, xml)
+
+            xml.Travelers {
+              xml.Traveler {
+                xml.AnonymousTraveler {
+                  xml.PTC(Quantity: 1).text "ADT"
+                }
+              }
+            }
+
+            xml.PointOfSale {
+              xml.Location {
+                xml.CountryCode_ data.hpath('PointOfSale/Location/CountryCode')
+                xml.CityCode_ data.hpath('PointOfSale/Location/CityCode')
+              }
+              xml.RequestTime(Zone: data.hpath('PointOfSale/RequestTime/Zone')).text data.hpath('PointOfSale/RequestTime')
+              xml.TouchPoint {
+                xml.Device {
+                  xml.Code_ data.hpath('PointOfSale/TouchPoint/Device/Code')
+                  xml.Definition_ data.hpath('PointOfSale/TouchPoint/Device/Definition')
+                  xml.Position {
+                    xml.Latitude_ data.hpath('PointOfSale/TouchPoint/Position/Latitude')
+                    xml.Longitude_ data.hpath('PointOfSale/TouchPoint/Position/Longitude')
+                    xml.NAC_ data.hpath('PointOfSale/TouchPoint/Position/NAC')
+                  }
+                }
+                xml.Event data.hpath('PointOfSale/TouchPoint/Event')
+              }
+            }
+
+            # Missing Preferences Block - TEMPORARY FIX FOR JRT!
+            xml.Preference {
+              xml.AirlinePreferences {
+                xml.Airline {
+                  xml.AirlineID_ data.hpath('Preference/AirlinePreferences/Airline/AirlineID')
+                }
+              }
+              xml.FarePreferences {
+                xml.Types {
+                  xml.Type {
+                    xml.Code_ data.hpath('Preference/FarePreferences/Types/Type/Code')
+                  }
+                }
+              }
+              xml.CabinPreferences {
+                xml.CabinType {
+                  xml.Code_ data.hpath('Preference/CabinPreferences/CabinType/Code')
+                  xml.Definition_ data.hpath('Preference/CabinPreferences/CabinType/Definition')
+                }
+              }
+            }
+
+            xml.Parameters {
+              xml.CurrCodes {
+                xml.CurrCode_ data.hpath('Parameters/CurrCodes/CurrCode')
+              }
+            }
+
+            # } # TEMPORARY FIX FOR JRT!
+            xml.Metadata {
+              xml.Other {
+                xml.OtherMetadata {
+                  xml.LanguageMetadatas {
+                    xml.LanguageMetadata(MetadataKey: "Display"){
+                      xml.Application_ "Display"
+                      xml.Code_ISO_ "en"
+                    }
+                  }
+                }
+              }
+            }
+          }
+
+        }
+      end
+
+
+      def to_xml
+        @message.doc.root.to_xml
+      end
+
 
     end
   end
