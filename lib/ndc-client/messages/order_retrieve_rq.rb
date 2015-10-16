253996a516a58421ef9d463a71a9e47e2aa61b18
Jorge Díaz <jorgedf@gmail.com>
2015-10-16 23:31:17 +0200

    OrderRetrieve added and tested.

---
+++ lib/ndc-client/messages/order_retrieve_rq.rb
@@ -0,0 +1,21 @@
+module NDCClient
+  module Messages
+
+    class OrderCreateRQ < Messages::Base
+
+      def initialize(params = {})
+        super(params)
+      end
+
+      def yield_core_query(data, xml)
+        xml.Query {
+          xml.Filters {
+            xml.OrderID(Owner: data.hpath('Query/Filters/OrderID/_Owner')) { xml.text data.hpath('Query/Filters/OrderID/__text') }
+          }
+        }
+      end
+
+    end
+
+  end
+end
---
+++ test/ndc_order_retrieve_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class NDCOrderRetrieveTest < Test::Unit::TestCase
+  extend Minitest::Spec::DSL
+
+  describe "Sends an valid OrderRetrieve request" do
+
+    ndc_config = YAML.load_file('test/config/ndc-iata-kronos.yml')
+    @@ndc_client = NDCClient::Base.new(ndc_config)
+
+    #TODO Should take OrderId from a CreateOrder Request
+
+    query_params = {
+      Query: {
+        Filters: {
+          OrderId: {
+            _Owner: "9A",
+            __Text: "F9A18I",
+          }
+        }
+      }
+    }
+
+    @@ndc_response = @@ndc_client.request(:OrderRetrieve, query_params)
+
+    test "OrderRetrieve response is valid" do
+      assert @@ndc_client.valid_response?
+    end
+
+    test "Document version is ok" do
+      refute_empty @@ndc_response.hpath('OrderViewRS/Document')
+      assert_equal @@ndc_response.hpath('OrderRetrieveRS/Document/ReferenceVersion'), "1.0"
+    end
+
+    test "Response includes Success element" do
+      refute_nil @@ndc_response["OrderViewRS"].has_key?("Success")
+    end
+
+  end
+
+end
