bdcc7c6aa766740aa75874b13b72dd96b13f1444
Jorge Díaz <jorgedf@gmail.com>
2015-10-24 01:29:41 +0200

    Fix OrderCreate message builder.

--- lib/ndc-client/messages/order_create_rq.rb
+++ lib/ndc-client/messages/order_create_rq.rb
@@ -8,10 +8,11 @@ module NDCClient
       end
 
       def yield_core_query(data, xml)
+
         xml.Query {
-          if data.hpath('Passengers').present?
+          if data.hpath('Query/Passengers').present?
             xml.Passengers {
-              data.hpath('Passengers').each do |passenger|
+              data.hpath('Query/Passengers').each do |passenger|
                 xml.Passenger((passenger.hpath('_ObjectKey').present? ? {ObjectKey: passenger.hpath('_ObjectKey')} : nil )) {
                   xml.PTC((passenger.hpath('PTC/_Quantity').present? ? {Quantity: passenger.hpath('PTC/_Quantity')} : nil )) { xml.text passenger.hpath('PTC/__text') }
                   xml.ResidenceCode_ passenger.hpath('ResidenceCode')
@@ -29,17 +30,23 @@ module NDCClient
                   if passenger.hpath('Contacts').present?
                     passenger.hpath('Contacts').each {|contact|
                       xml.Contact{
-                        xml.EmailContact {
-                          xml.Address_ contact.hpath('EmailContact/Address')
-                        }
-                        xml.PhoneContact {
-                          xml.Application_ contact.hpath('PhoneContact/Application')
-                          xml.Number_ contact.hpath('PhoneContact/Number')
-                        }
-                        xml.AddressContact {
-                          xml.Application_ contact.hpath('AddressContact/Application')
-                          xml.Number_ contact.hpath('PhoneContact/Number')
-                        }
+                        if contact.hpath('Contact/EmailContact').present?
+                          xml.EmailContact {
+                            xml.Address_ contact.hpath('Contact/EmailContact/Address')
+                          }
+                        end
+                        if contact.hpath('Contact/PhoneContact').present?
+                          xml.PhoneContact {
+                            xml.Application_ contact.hpath('Contact/PhoneContact/Application')
+                            xml.Number_ contact.hpath('Contact/PhoneContact/Number')
+                          }
+                        end
+                        if contact.hpath('Contact/AddressContact').present?
+                          xml.AddressContact {
+                            xml.Application_ contact.hpath('Contact/AddressContact/Application')
+                            xml.Number_ contact.hpath('Contact/PhoneContact/Number')
+                          }
+                        end
                       }
                     }
                   end
@@ -63,33 +70,32 @@ module NDCClient
             }
           end
 
-
           xml.OrderItems {
             xml.ShoppingResponse {
-              xml.Owner_ data.hpath('OrderItems/ShoppingResponse/Owner')
-              xml.ResponseID_ data.hpath('OrderItems/ShoppingResponse/ResponseID')
-              if data.hpath('OrderItems/ShoppingResponse/Offers').present?
+              xml.Owner_ data.hpath('Query/OrderItems/ShoppingResponse/Owner')
+              xml.ResponseID_ data.hpath('Query/OrderItems/ShoppingResponse/ResponseID')
+              if data.hpath('Query/OrderItems/ShoppingResponse/Offers').present?
                 xml.Offers {
-                  data.hpath('OrderItems/ShoppingResponse/Offers').each do |offer|
+                  data.hpath('Query/OrderItems/ShoppingResponse/Offers').each do |offer|
                     xml.Offer {
-                      xml.OfferID((offer.hpath('OfferID/_Owner').present? ? {Owner: offer.hpath('OfferID/_Owner')} : nil)) {xml.text offer.hpath('OfferID/__text')}
+                      xml.OfferID((offer.hpath('Offer/OfferID/_Owner').present? ? {Owner: offer.hpath('Offer/OfferID/_Owner')} : nil)) {xml.text offer.hpath('Offer/OfferID/__text')}
                       if offer.hpath('Offer/OfferItems').present?
                         xml.OfferItems {
                           offer.hpath('Offer/OfferItems').each do |offer_item|
                             xml.OfferItem {
-                              xml.OfferItemID((offer_item.hpath('OfferItemID/_Owner').present? ? {Owner: offer_item.hpath('OfferItemID/_Owner')} : nil)) {xml.text offer_item.hpath('OfferItemID/__text')}
-                              if offer_item.hpath('Passengers').present?
+                              xml.OfferItemID((offer_item.hpath('OfferItem/OfferItemID/_Owner').present? ? {Owner: offer_item.hpath('OfferItem/OfferItemID/_Owner')} : nil)) {xml.text offer_item.hpath('OfferItem/OfferItemID/__text')}
+                              if offer_item.hpath('OfferItem/Passengers').present?
                                 xml.Passengers {
-                                  offer_item.hpath('Passengers').each do |passenger|
+                                  offer_item.hpath('OfferItem/Passengers').each do |passenger|
                                     xml.PassengerReference_ passenger.hpath('PassengerReference')
                                   end
                                 }
                               end
-                              if offer_item.hpath('AssociatedServices').present?
+                              if offer_item.hpath('OfferItem/AssociatedServices').present?
                                 xml.AssociatedServices {
-                                  offer_item.hpath('AssociatedServices').each do |associated_service|
+                                  offer_item.hpath('OfferItem/AssociatedServices').each do |associated_service|
                                     xml.AssociatedService {
-                                      xml.ServiceID((associated_service.hpath('ServiceID/_Owner').present? ? {Owner: associated_service.hpath('ServiceID/_Owner')} : nil)) {xml.text associated_service.hpath('ServiceID/__text')}
+                                      xml.ServiceID((associated_service.hpath('AssociatedService/ServiceID/_Owner').present? ? {Owner: associated_service.hpath('AssociatedService/ServiceID/_Owner')} : nil)) {xml.text associated_service.hpath('AssociatedService/ServiceID/__text')}
                                     }
                                   end
                                 }
@@ -105,49 +111,52 @@ module NDCClient
             }
           }
 
-
-
-
-          xml.Payments {
-            data.hpath('Payments').each do |payment|
-              xml.Payment {
-                xml.Method {
-                  xml.PaymentCard {
-                    xml.CardCode_ payment.hpath('Method/PaymentCard/CardCode')
-                    xml.CardNumber_ payment.hpath('Method/PaymentCard/CardNumber')
-                    xml.SeriesCode_ payment.hpath('Method/PaymentCard/SeriesCode')
-                    xml.EffectiveExpireDate {
-                      xml.Effective_ payment.hpath('Method/PaymentCard/EffectiveExpireDate/Effective')
-                    }
-                    xml.Amount((payment.hpath('Amount/_Taxable').present? ? {Taxable: payment.hpath('Amount/_Taxable')} : nil)) {xml.text payment.hpath('Amount/__text')}
-                    xml.Payer {
-                      xml.Name_ payment.hpath('Payer/Name')
-                      xml.Surname_ payment.hpath('Amount/Surname')
-                    }
-                    if payment.hpath('Contacts').present?
-                      xml.Contacts {
-                        payment.hpath('Contacts').each do |contact|
-                          xml.Contact {
-                            xml.AddressContact {
-                              xml.Street_ contact.hpath('AddressContact/Street')
-                              xml.CityName {
-                                xml.CityCode_ contact.hpath('AddressContact/CityName/CityCode')
-                              }
-                              xml.PostalCode_ contact.hpath('AddressContact/PostalCode')
-                              xml.CountryCode_ contact.hpath('AddressContact/CountryCode')
-                              xml.EmailContact {
-                                xml.Address_ contact.hpath('AddressContact/EmailContact/Address')
-                              }
-                            }
-                          }
-                        end
+          if data.hpath('Query/Payments').present?
+            xml.Payments {
+              data.hpath('Query/Payments').each do |payment|
+                xml.Payment {
+                  xml.Method {
+                    xml.PaymentCard {
+                      xml.CardCode_ payment.hpath('Payment/Method/PaymentCard/CardCode')
+                      xml.CardNumber_ payment.hpath('Payment/Method/PaymentCard/CardNumber')
+                      xml.SeriesCode_ payment.hpath('Payment/Method/PaymentCard/SeriesCode')
+                      xml.EffectiveExpireDate {
+                        xml.Effective_ payment.hpath('Payment/Method/PaymentCard/EffectiveExpireDate/Effective')
                       }
-                    end
+                      xml.Amount((payment.hpath('Payment/Amount/_Taxable').present? ? {Taxable: payment.hpath('Payment/Amount/_Taxable')} : nil)) {xml.text payment.hpath('Payment/Amount/__text')}
+                      xml.Payer {
+                        xml.Name_ payment.hpath('Payment/Payer/Name')
+                        xml.Surname_ payment.hpath('Payment/Amount/Surname')
+                      }
+                      if payment.hpath('Payment/Payer/Contacts').present?
+                        xml.Contacts {
+                          payment.hpath('Payment/Payer/Contacts').each do |contact|
+                            xml.Contact {
+                              if contact.hpath('Contact/AddressContact').present?
+                                xml.AddressContact {
+                                  xml.Street_ contact.hpath('Contact/AddressContact/Street')
+                                  xml.CityName {
+                                    xml.CityCode_ contact.hpath('Contact/AddressContact/CityName/CityCode')
+                                  }
+                                  xml.PostalCode_ contact.hpath('Contact/AddressContact/PostalCode')
+                                  xml.CountryCode_ contact.hpath('Contact/AddressContact/CountryCode')
+                                }
+                              end
+                              if contact.hpath('Contact/EmailContact').present?
+                                xml.EmailContact {
+                                  xml.Address_ contact.hpath('Contact/AddressContact/EmailContact/Address')
+                                }
+                              end
+                            }
+                          end
+                        }
+                      end
+                    }
                   }
                 }
-              }
-            end
-          }
+              end
+            }
+          end
         }
       end
 
