a25d1f1fefcaab49a643ece0aec17ef695decee1
Jorge Díaz <jorgedf@gmail.com>
2016-02-12 11:44:49 +0100

    Add some ServiceList tests.

--- test/ndc_service_list_test.rb
+++ test/ndc_service_list_test.rb
@@ -3,9 +3,27 @@ require 'test_helper'
 class NDCServiceListTest < Test::Unit::TestCase
   extend Minitest::Spec::DSL
 
-  describe "Sends an valid ServiceList request" do
+  @@ndc_client = NDCClient::Base.new(@@ndc_config)
 
-    @@ndc_client = NDCClient::Base.new(@@ndc_config)
+  describe "Sends an invalid ServiceList request (random ShoppingResponseID)" do
+
+    @response_id = Digest::MD5.new.hexdigest
+
+    query_params = {
+      ShoppingResponseIDs: {
+        ResponseID: @response_id
+      }
+    }
+
+    @@ndc_response = @@ndc_client.request(:ServiceList, query_params)
+
+    test "ServiceList response is invalid" do
+      refute @@ndc_client.valid_response?
+    end
+
+  end
+
+  describe "Sends an valid ServiceList request" do
 
     @@ndc_response = @@ndc_client.request(:AirShopping, NDCAirShoppingTest::VALID_REQUEST_PARAMS)
     @response_id = @@ndc_response.hpath('AirShoppingRS/ShoppingResponseIDs/ResponseID')
@@ -28,7 +46,11 @@ class NDCServiceListTest < Test::Unit::TestCase
     end
 
     test "Response includes Success element" do
-      refute_nil @@ndc_response["ServiceListRS"].has_key?("Success")
+      assert_true @@ndc_response["ServiceListRS"].has_key?("Success")
+    end
+
+    test "Response includes ServiceList listing" do
+      refute_empty @@ndc_response["ServiceListRS/DataLists"].has_key?("ServiceList")
     end
 
   end
