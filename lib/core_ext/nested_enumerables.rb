8b3d4477f1863f0f5ea253e16f6c1b5e13c7e0f5
Jorge Díaz <jorgedf@gmail.com>
2016-02-19 03:28:57 +0100

    Add hpath? to Hash to check the existing of elements.

--- lib/core_ext/nested_enumerables.rb
+++ lib/core_ext/nested_enumerables.rb
@@ -10,12 +10,19 @@ module Enumerable
 
 end
 
+class String
+
+  def is_number?
+    true if Float(self) rescue false
+  end
+
+end
+
 
 class Hash
 
   def access(path)
     value = self
-
     path.to_s.split('/').each do |p|
       if p.to_i.to_s == p
         value = value[p.to_i]
@@ -24,10 +31,30 @@ class Hash
       end
       break if value.nil?
     end
-
     value
   end
 
+  def access?(path)
+    hash = self
+    keys = path.to_s.split('/')
+    keys.each_with_index do |k, i|
+      if k.is_number?
+        hash = hash[k.to_i]
+      else
+        k = k.to_sym
+        if i == keys.size - 1
+          hash = hash.has_key?(k)
+        else
+          hash = hash[k]
+        end
+      end
+      break if hash.is_a?(TrueClass) || hash.is_a?(FalseClass) || !hash.is_a?(Hash)
+    end
+
+    hash == true
+  end
+
   alias :hpath :access
+  alias :hpath? :access?
 
 end
