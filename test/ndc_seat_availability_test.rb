e6dc6f0f19c1813888b2f23e8e29db620756d8f8
zaali kavelashvili <zaali@live.com>
2016-03-04 15:49:18 +0400

    Comment seat invalid params

--- test/ndc_seat_availability_test.rb
+++ test/ndc_seat_availability_test.rb
@@ -3,6 +3,127 @@ require_relative 'test_helper'
 class NDCSeatAvailabilityTest < Test::Unit::TestCase
   extend Minitest::Spec::DSL
 
+  #describe "Sends an invalid SeatAvailability" do
+    #let(:invalid_query_params) {
+    #{
+      #Query: {
+        #OriginDestination: {
+          #OriginDestinationReferences: 'OD1'
+        #}
+      #},
+      #DataList: {
+        #OriginDestinationList: {
+          #OriginDestination: {
+            #_OriginDestinationKey: 'OD1',
+            #DepartureCode: "SXF",
+            #ArrivalCode: "MAD"
+          #}
+        #},
+        #FlightList: [
+          #{
+            #Flight: {
+              #_FlightKey: 'FL1',
+              #Journey: {
+                #Time: 'PT6H55M'
+              #},
+              #SegmentReferences: 'SEG1 SEG2'
+            #}
+          #}
+        #],
+        #FlightSegmentList: [
+          #{
+            #FlightSegment: {
+              #_SegmentKey: "SEG1",
+              #Departure: {
+                #AirportCode: "BLA",
+                #Date: "2016-03-01",
+                #Time: "20:30"
+              #},
+              #Arrival: {
+                #AirportCode: "MAD",
+                #Date: "2016-03-01",
+                #Time: "23:10",
+                #AirportName: "Madrid Adolfo Suárez International Airport"
+              #},
+              #MarketingCarrier: {
+                #AirlineID: "FA",
+                #Name: "Kronos Air",
+                #FlightNumber: "809"
+              #},
+              #OperatingCarrier: {
+                #AirlineID: "FA",
+                #Name: "Kronos Air",
+                #FlightNumber: "809"
+              #},
+              #Equipment: {
+                #AircraftCode: "31F",
+                #Name: "E95 EMBRAER 195 JET"
+              #},
+              #ClassOfService: {
+                #Code: "M"
+              #},
+              #FlightDetail: {
+                #FlightDuration: {
+                  #Value: "PT2H10M"
+                #}
+              #},
+            #}
+          #},
+          #{
+            #FlightSegment: {
+              #_SegmentKey: "SEG2",
+              #Departure: {
+                #AirportCode: "SXF",
+                #Date: "2016-03-01",
+                #Time: "20:30",
+                #AirportName: "Berlin Schönefeld"
+              #},
+              #Arrival: {
+                #AirportCode: "MAD",
+                #Date: "2016-05-05",
+                #Time: "23:10",
+                #AirportName: "Madrid Adolfo Suárez International Airport"
+              #},
+              #MarketingCarrier: {
+                #AirlineID: "FA",
+                #Name: "Kronos Air",
+                #FlightNumber: "890"
+              #},
+              #OperatingCarrier: {
+                #AirlineID: "FA",
+                #Name: "Kronos Air",
+                #FlightNumber: "890"
+              #},
+              #Equipment: {
+                #AircraftCode: "31F",
+                #Name: "321 - AIRBUS INDUSTRIE A321 JET"
+              #},
+              #ClassOfService: {
+                #Code: "M"
+              #},
+              #FlightDetail: {
+                #FlightDuration: {
+                  #Value: "PT3H5M"
+                #}
+              #}
+            #}
+          #}
+        #]
+      #}
+    #}}
+
+    #setup do
+      #@ndc_client = NDCClient::Base.new(@@ndc_config)
+    #end
+
+    #test "SeatAvailability request raises Processing error" do
+      #assert_raises(NDCClient::NDCErrors::UnknownNDCProcessingError) {
+        #@@ndc_response = @ndc_client.request(:SeatAvailability, invalid_query_params)
+      #}
+    #end
+
+  #end
+
   describe "Sends an valid SeatAvailability request" do
 
     let(:query_params) {
@@ -38,13 +159,13 @@ class NDCSeatAvailabilityTest < Test::Unit::TestCase
               Departure: {
                 AirportCode: "SXF",
                 Date: "2016-03-01",
-                Time: "20:30"
+                Time: "12:00"
               },
               Arrival: {
-                AirportCode: "MAD",
+                AirportCode: "FRA",
                 Date: "2016-03-01",
-                Time: "23:10",
-                AirportName: "Madrid Adolfo Suárez International Airport"
+                Time: "13:10",
+                AirportName: "Frankfurt International Airport"
               },
               MarketingCarrier: {
                 AirlineID: "FA",
@@ -74,15 +195,15 @@ class NDCSeatAvailabilityTest < Test::Unit::TestCase
             FlightSegment: {
               _SegmentKey: "SEG2",
               Departure: {
-                AirportCode: "SXF",
+                AirportCode: "FRA",
                 Date: "2016-03-01",
-                Time: "20:30",
-                AirportName: "Berlin Schönefeld"
+                Time: "14:00",
+                AirportName: "Frankfurt International Airport"
               },
               Arrival: {
                 AirportCode: "MAD",
                 Date: "2016-05-05",
-                Time: "23:10",
+                Time: "16:35",
                 AirportName: "Madrid Adolfo Suárez International Airport"
               },
               MarketingCarrier: {
